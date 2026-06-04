e7634b3d234b14b913f70364e7b656f83f248e07
mist3rth <mist3rth@users.noreply.github.com>
2026-06-04 11:17:17 +0200

    perf(problem): optimisation des imports GSAP pour ameliorer le tree-shaking

--- src/components/sections/Section01Problem.tsx
+++ src/components/sections/Section01Problem.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { motion, useInView } from "motion/react";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import { getAssetPath } from "../../utils/assets";
